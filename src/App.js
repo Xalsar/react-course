0c640c1fce0a223f166f826198c7b71e6bcf851f
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-05-01 13:32:09 +0200

    Lecture 58

--- src/App.js
+++ src/App.js
@@ -19,14 +19,16 @@ class App extends Component {
     this.setState({ persons: persons })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 29 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 29 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id)
+    const person = { ...this.state.persons[personIndex] }
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({ persons })
   }
 
   tagglePersonsHandler = () => {
@@ -46,7 +48,8 @@ class App extends Component {
               click={() => this.deletePersonhandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>)
     }
