1587b2d6ea412c5b2d0b915b3c13ce965b3627cf
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-05-13 08:59:23 +0200

    Lecture 102

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Aux from '../../../hoc/Aux'
+import React, { Fragment } from 'react'
 // import styles from './Person.module.css'
 const person = (props) => {
     console.log('[Person.js] rendering...')
 
     return (
-        <Aux>
+        <Fragment>
             <p onClick={props.click}>I'm {props.name}! and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </Aux>
+        </Fragment>
     )
 }
 
