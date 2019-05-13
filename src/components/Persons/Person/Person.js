7682995cd31e5c5fd8cee5cd6bc47e8a89ddc5d1
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-05-13 17:48:45 +0200

    Lecture 107

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import withCass from '../../../hoc/withClass'
 import styles from './Person.module.css'
+import PropTypes from 'prop-types'
 
 const person = (props) => {
     console.log('[Person.js] rendering...')
@@ -12,6 +13,13 @@ const person = (props) => {
             <input type="text" onChange={props.changed} value={props.name} />
         </Fragment>
     )
+}
+
+person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
 
 export default withCass(person, styles.Person)
