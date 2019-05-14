1c3b5c44457426c0f8e10c02ba93899e7b6aaf38
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-05-14 10:22:08 +0200

    Lecture 109

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import classes from './Cockpit.module.css'
 
 const Cockpit = (props) => {
+  const toggleBtnRef = useRef(null)
+
   useEffect(() => {
     console.log('[Cockpit.js] useEffect')
     // Http requests
+    toggleBtnRef.current.click()
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect')
     }
@@ -37,6 +40,7 @@ const Cockpit = (props) => {
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working</p>
       <button
+        ref={toggleBtnRef}
         className={btnClass}
         onClick={props.clicked}
       >Toggle persons!</button>
